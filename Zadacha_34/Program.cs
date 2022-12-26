aa0e4c683d0a92e92e7ab9f34b80c8fce79c6d75
Vadim V P <Everestxxx555@yandex.ru>
2022-12-26 20:01:09 +0300

    Коммит2 Zadacha_34

--- Zadacha_34/Program.cs
+++ Zadacha_34/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трехзначными
+// числами. Напишите программу, которая покажет количество четных чисел в массиве.
+
+Console.Clear();
+
+int numberSchet = new Random().Next(5, 16);
+
+int[] ArrayData(int number)
+{
+   int[] array = new int[number];
+   int i = 0;
+  for(i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(100, 1000);
+  } 
+  return array;
+}
+
+void ArrayPozitivNumber(int[] arrayP)
+{
+    int sumPozitiv = 0;
+    for(int i = 0; i < arrayP.Length; i++)
+    {
+        if(arrayP[i] % 2 == 0)
+        {
+           sumPozitiv += 1; 
+        }
+    }
+    Console.Write($"{sumPozitiv} ");
+}
+
+
+int[] array = ArrayData(numberSchet);
+Console.WriteLine('[' + string.Join(",", array) + ']');
+ArrayPozitivNumber(array);
